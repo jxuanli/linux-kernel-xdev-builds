c1659cd05be21a2de160006df3c9815001570f52
jxuanli <junxua01@student.ubc.ca>
2026-01-01 14:07:43 -0800

    a

---
+++ parse_yml.py
@@ -0,0 +1,13 @@
+import yaml, os
+
+f = open(os.environ["CONFIG_FILE"])
+config = yaml.safe_load(f)
+f = open(os.environ["GITHUB_OUTPUT"], "w")
+f.write(f"version={config['version']}\n")
+f.write(f"ktype={config['type']}\n")
+frag = "frags/" + ".".join(os.environ["CONFIG_FILE"].split("/")[-1].split(".")[:-1]) + ".config"
+f.write(f"frag={frag}\n")
+f = open(frag, "w+")
+kernel_configs = config["configs"]
+for config, val in kernel_configs.items():
+    f.write(f"{config}={val}")
